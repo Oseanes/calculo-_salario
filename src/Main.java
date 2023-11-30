47a435e3f6c6538eddfee6b87ecf6717f6dc1cb5
oseanes <oseanes.farias@gmail.com>
2023-11-29 21:33:23 -0300

    Implementação completa do Projeto I

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,105 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+/*  Faça um programa que receba 5 salários brutos de funcionários, sabendo-se
+    que são descontados Imposto de Renda e INSS, calcule e mostre o total do salário
+    líquido no referido mês.
+
+        Obs.: Salário Bruto - Descontos = Salário Líquido.
+
+        A saída do programa deve fornecer as seguintes informações:
+
+        Salário bruto.
+        Quanto pagou ao INSS.
+        Quanto pagou de Imposto de Renda.
+        Salário líquido.
+*/
+
+
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {g
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) {
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        double salario;
+        double impostoRenda;
+        double inss;
+        double salarioLiquido;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Informe o salário:");
+                salario = Double.parseDouble(scanner.nextLine());
+
+                impostoRenda = calculaImpostoRenda(salario);
+                inss = calculaInss(salario);
+                salarioLiquido = salario - (impostoRenda + inss);
+
+                printInf(salario, salarioLiquido, impostoRenda, inss );
+            }
+        } catch (RuntimeException ex){
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    public static double calculaImpostoRenda(double salario) {
+        return Math.round(salario * impostoRenda(salario) * 100.00)/100.00;
+    }
+
+    public static double calculaInss(double salario) {
+        return Math.round( salario * inss(salario)* 100.00)/100.00;
+    }
+
+    public static void printInf(double salario, double salarioLiquido, double impostoRenda, double inss) {
+        System.out.println(
+                "Salário bruto: " + salario + "\n" +
+                        "Valor a ser pagou ao INSS: " + inss + "\n" +
+                        "Valor a ser pagou de Imposto de Renda:" + impostoRenda + "\n" +
+                        "Salário líquido: " + salarioLiquido
+        );
+    }
+
+    private static double impostoRenda(double salario) {
+
+        if (salario >= 4664.68) {
+            return 0.275;
+        } else if (salario > 3751.06) {
+            return 0.225;
+        } else if (salario > 2826.66) {
+            return 0.15;
+        } else if (salario > 1903.98) {
+            return 0.075;
+        } else {
+            return 0.0;
+        }
+    }
+
+    private static double inss(double salario) {
+
+        if (salario > 7087.22) {
+            return 0.0;
+        } else if (salario > 3641.03) {
+            return 0.14;
+        } else if (salario > 2427.35) {
+            return 0.12;
+        } else if (salario > 1212.00) {
+            return 0.09;
+        } else {
+            return 0.075;
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
